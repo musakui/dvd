93b7bf068b1c3945d2cb53c09ef8701c1e6a94ab
Shun Git <kwokshungit@gmail.com>
2020-04-25 13:24:01 +0800

    fix indentation

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,12 @@ import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 
 export default [
-	{
-		input: 'index.js',
+  {
+    input: 'index.js',
     plugins: [
       resolve({ browser: true, modulesOnly: true }),
       terser(),
     ],
-		output: [ { format: 'esm', file: pkg.main } ]
-	},
+    output: [ { format: 'esm', file: pkg.main } ]
+  },
 ]
